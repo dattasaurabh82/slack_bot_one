e55c4d737acf53a05a5055904c580a1622b70ee3
Saurabh Datta <dattasaurabh82@users.noreply.github.com>
2017-10-18 14:50:42 +0800

    testing messaging with my username

--- server.js
+++ server.js
@@ -8,21 +8,41 @@ var port = process.env.PORT || 1337;
 app.use(bodyParser.urlencoded({ extended: true }));
  
 // test route
-app.get('/', function (req, res) { 
-    res.status(200).send('Hello world!'); 
-});
+// app.get('/', function (req, res) { 
+//     res.status(200).send('Hello world!'); 
+// });
+
+var replyCounter = 0;
 
 app.post('/hello', function (req, res, next) {
   var userName = req.body.user_name;
   var botPayload = {
-    text : 'Hello ' + userName + ', welcome to Devdactic Slack channel! I\'ll be your guide.'
+    text : 'Hello ' + userName + ', How are you doing?'
   };
   // Loop otherwise..
-  if (userName !== 'slackbot') {
+  if (userName == 'dattasaurabh82') {
     return res.status(200).json(botPayload);
+    replyCounter++;
   } else {
     return res.status(200).end();
   }
+});
+
+app.post('/', function(req, res, next){
+  var userName = req.body.user_name;
+  var botPayloadOne = {
+    text : 'Nothing matters'
+  };
+  // Loop otherwise..
+  if (userName == 'dattasaurabh82') {
+    if(replyCounter == 1){
+      return res.status(200).json(botPayloadOne);
+      replyCounter = 0;  
+    }
+  } else {
+    return res.status(200).end();
+  }
+
 });
 
 app.listen(port, function () {
