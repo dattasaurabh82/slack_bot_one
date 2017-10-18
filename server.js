364204a010a79c8b109eb6f6c119265e8de64c85
Saurabh Datta <dattasaurabh82@users.noreply.github.com>
2017-10-18 14:54:38 +0800

    testing messaging with any post under my usrname

--- server.js
+++ server.js
@@ -6,43 +6,20 @@ var port = process.env.PORT || 1337;
  
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
- 
-// test route
-// app.get('/', function (req, res) { 
-//     res.status(200).send('Hello world!'); 
-// });
 
 var replyCounter = 0;
 
-app.post('/hello', function (req, res, next) {
+app.post('/', function (req, res, next) {
   var userName = req.body.user_name;
   var botPayload = {
-    text : 'Hello ' + userName + ', How are you doing?'
+    text : 'Hello ' + userName + ', It doesn\'t matter'
   };
   // Loop otherwise..
   if (userName == 'dattasaurabh82') {
     return res.status(200).json(botPayload);
-    replyCounter++;
   } else {
     return res.status(200).end();
   }
-});
-
-app.post('/', function(req, res, next){
-  var userName = req.body.user_name;
-  var botPayloadOne = {
-    text : 'Nothing matters'
-  };
-  // Loop otherwise..
-  if (userName == 'dattasaurabh82') {
-    if(replyCounter == 1){
-      return res.status(200).json(botPayloadOne);
-      replyCounter = 0;  
-    }
-  } else {
-    return res.status(200).end();
-  }
-
 });
 
 app.listen(port, function () {
